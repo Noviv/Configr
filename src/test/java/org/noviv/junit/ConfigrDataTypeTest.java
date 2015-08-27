545722394566ebe8ca59dfffcf421c0041c26708
Noviv <matthewwebb65@yahoo.com>
2015-08-26 21:37:06 -0500

    tested ConfigrDataType

--- src/test/java/org/noviv/junit/ConfigrDataTypeTest.java
+++ src/test/java/org/noviv/junit/ConfigrDataTypeTest.java
@@ -3,11 +3,8 @@ package org.noviv.junit;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.noviv.configr.data.ConfigrDataType;
+import static org.noviv.configr.data.ConfigrDataType.*;
 
-/**
- *
- * @author Matthew
- */
 public class ConfigrDataTypeTest {
 
     /**
@@ -16,25 +13,9 @@ public class ConfigrDataTypeTest {
     @Test
     public void testValues() {
         System.out.println("values");
-        ConfigrDataType[] expResult = null;
+        ConfigrDataType[] expResult = {STRING, BOOLEAN, INTEGER, DOUBLE, NULL};
         ConfigrDataType[] result = ConfigrDataType.values();
         assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of valueOf method, of class ConfigrDataType.
-     */
-    @Test
-    public void testValueOf() {
-        System.out.println("valueOf");
-        String name = "";
-        ConfigrDataType expResult = null;
-        ConfigrDataType result = ConfigrDataType.valueOf(name);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -43,12 +24,12 @@ public class ConfigrDataTypeTest {
     @Test
     public void testGetName() {
         System.out.println("getName");
-        ConfigrDataType instance = null;
-        String expResult = "";
-        String result = instance.getName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        String[] names = {"String", "Boolean", "Integer", "Double", "Null"};
+        int index = 0;
+        for (ConfigrDataType t : ConfigrDataType.values()) {
+            assertEquals(t.getName(), names[index]);
+            index++;
+        }
     }
 
     /**
@@ -57,12 +38,12 @@ public class ConfigrDataTypeTest {
     @Test
     public void testGetDefault() {
         System.out.println("getDefault");
-        ConfigrDataType instance = null;
-        Object expResult = null;
-        Object result = instance.getDefault();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Object[] defaults = {"", false, 0, 0.0, null};
+        int index = 0;
+        for (ConfigrDataType t : ConfigrDataType.values()) {
+            assertEquals(t.getDefault(), defaults[index]);
+            index++;
+        }
     }
 
 }
